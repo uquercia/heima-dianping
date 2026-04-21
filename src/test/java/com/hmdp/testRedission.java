c829c6b9cfddc608641918b51a4203f652c18320
ChenJiaJun <914838067@qq.com>
2026-04-21 19:59:36 +0800

    feat redisson可重入锁单测编写

--- src/test/java/com/hmdp/testRedission.java
+++ src/test/java/com/hmdp/testRedission.java
@@ -1,5 +1,6 @@
 package com.hmdp;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
@@ -8,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import javax.annotation.Resource;
 import java.util.concurrent.TimeUnit;
 
+@Slf4j
 @SpringBootTest
 public class testRedission {
     @Resource
@@ -30,4 +32,29 @@ public class testRedission {
             lock.unlock();
         }
     }
+
+    @Test
+    void testReentrantLock() {
+        RLock lock = redissonClient.getLock("test:reentrant");
+        try {
+            // 第一次获取锁
+            boolean isLock1 = lock.tryLock();
+            if (isLock1) {
+                log.info("第一次加锁成功");
+                try {
+                    // 在同一个线程中，第二次尝试获取同一把锁
+                    boolean isLock2 = lock.tryLock();
+                    if (isLock2) {
+                        log.info("第二次加锁成功（触发重入）");
+                        // 此时去 Redis 查看，该 Hash 的 value 应该是 2
+                    }
+                } finally {
+                    if (lock.isHeldByCurrentThread()) lock.unlock();
+                }
+            }
+        } finally {
+            if (lock.isHeldByCurrentThread()) lock.unlock();
+        }
+    }
+
 }
